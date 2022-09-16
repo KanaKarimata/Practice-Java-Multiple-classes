7419aeb1f5bb15283d23764158e7f94e86cee19d
KanaKarimata <kana.lt22@gmail.com>
2022-09-16 20:58:04 +0900

    Split the Spurce File

--- src/com/practice/MultipleClasses.java
+++ src/com/practice/MultipleClasses.java
@@ -3,17 +3,11 @@ package com.practice;
 public class MultipleClasses {
     public static void main(String[] args) {
         int a = 10;
-        int b = 2;
-        int total = add(a, b);
-        int delta = minus(a, b);
+        int b = 8;
+        int total = CalcLogic.add(a, b);
+        int delta = CalcLogic.minus(a, b);
         System.out.println("total>" + total + ", delta>" + delta);
     }
 
-    public static int add( int a, int b) {
-        return (a + b);
-    }
 
-    public static int minus(int a, int b) {
-        return (a - b);
-    }
 }
